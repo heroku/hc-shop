f992e4cce126246d69192488938f52099ee2e398
Marc Sibson <sibson@gmail.com>
2014-10-24 09:09:35 -0700

    create SFDC objects for new orders

--- salesforce/models.py
+++ salesforce/models.py
@@ -1,23 +1,16 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Remove `managed = False` lines if you wish to allow Django to create and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
-
-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from cartridge.shop.models import Order
 
 
 class TriggerLog(models.Model):
     id = models.IntegerField(primary_key=True)
     table_name = models.CharField(max_length=40, blank=True)
-    field_c5_source = models.CharField(db_column='_c5_source', max_length=40, blank=True) # Field renamed because it started with '_'.
+    field_c5_source = models.CharField(db_column='_c5_source', max_length=40, blank=True)
     action = models.CharField(max_length=7, blank=True)
     created_at = models.DateTimeField(blank=True, null=True)
     record_id = models.IntegerField(blank=True, null=True)
@@ -25,13 +18,14 @@ class TriggerLog(models.Model):
     state = models.CharField(max_length=8, blank=True)
     sf_result = models.IntegerField(blank=True, null=True)
     sf_message = models.CharField(max_length=1024, blank=True)
+
     class Meta:
         managed = False
         db_table = '_trigger_log'
 
 
 class Lead(models.Model):
-    field_c5_source = models.CharField(db_column='_c5_source', max_length=18, blank=True) # Field renamed because it started with '_'.
+    field_c5_source = models.CharField(db_column='_c5_source', max_length=18, blank=True)
     firstname = models.CharField(max_length=40, blank=True)
     lastmodifieddate = models.DateTimeField(blank=True, null=True)
     company = models.CharField(max_length=255, blank=False)
@@ -40,26 +34,38 @@ class Lead(models.Model):
     lastname = models.CharField(max_length=80, blank=False)
     sfid = models.CharField(unique=True, max_length=18, blank=True)
     isdeleted = models.NullBooleanField()
+
+    def __unicode__(self):
+        return '{} - {}/{}'.format(self.email, self.id, self.sfid)
+
+    @property
+    def name(self):
+        return ' '.join(self.firstname, self.lastname)
+
     class Meta:
         managed = False
         db_table = 'lead'
 
 
 class Account(models.Model):
-    field_c5_source = models.CharField(db_column='_c5_source', max_length=18, blank=True) # Field renamed because it started with '_'.
+    field_c5_source = models.CharField(db_column='_c5_source', max_length=18, blank=True)
     name = models.CharField(max_length=255, blank=False)
     accountnumber = models.CharField(max_length=40, blank=True)
     lastmodifieddate = models.DateTimeField(blank=True, null=True)
     createddate = models.DateTimeField(blank=True, null=True)
     sfid = models.CharField(unique=True, max_length=18, blank=True)
     isdeleted = models.NullBooleanField()
+
     class Meta:
         managed = False
         db_table = 'account'
 
+    def __unicode__(self):
+        return '{} - {}/{}'.format(self.name, self.id, self.sfid)
+
 
 class Contact(models.Model):
-    field_c5_source = models.CharField(db_column='_c5_source', max_length=18, blank=True) # Field renamed because it started with '_'.
+    field_c5_source = models.CharField(db_column='_c5_source', max_length=18, blank=True)
     lastmodifieddate = models.DateTimeField(blank=True, null=True)
     firstname = models.CharField(max_length=40, blank=True)
     lastname = models.CharField(max_length=80, blank=False)
@@ -72,6 +78,23 @@ class Contact(models.Model):
         managed = False
         db_table = 'contact'
 
-
     def __unicode__(self):
         return '{} - {}/{}'.format(self.email, self.id, self.sfid)
+
+    @property
+    def name(self):
+        return ' '.join(self.firstname, self.lastname)
+
+
+@receiver(post_save, sender=Order, dispatch_uid='salesforce_order_post_save')
+def order_post_save(sender, **kwargs):
+    """ Anytime we get a new order lets create new SFDC objects too
+    """
+    order = kwargs.get('instance')
+    email = order.billing_detail_email
+    account, _ = Account.objects.get_or_create(name=email)
+
+    # XXX link contact to account
+    contact, _ = Contact.objects.get_or_create(email=email)
+
+    return account, contact
